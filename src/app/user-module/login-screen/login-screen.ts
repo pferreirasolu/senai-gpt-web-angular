2e12b719d16ed171432deb33c350bf08c01a75f9
Pferreirasolu <pedrohg.ferreira@hotmail.com>
2025-09-04 21:51:25 -0300

    Finalização acessos de login e senha

--- src/app/user-module/login-screen/login-screen.ts
+++ src/app/user-module/login-screen/login-screen.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-screen',
   templateUrl: './login-screen.html',
-  styleUrls: ['./login-screen.css']
+  styleUrls: ['./login-screen.css'],
+  imports: [ReactiveFormsModule]
 })
 export class LoginScreen {
   //quando a tela iniciar
@@ -15,17 +16,51 @@ export class LoginScreen {
   constructor(private fb: FormBuilder) {
 
     this.loginForm = this.fb.group({
-      email:["",[Validators.required]],
-      password: ["",[Validators.required]]
+      email: ["", [Validators.required]],
+      password: ["", [Validators.required]]
     })
-  
+
   }
 
-  onLoginClick(){
-
-    alert("Botão de login clicado.");
+  async onLoginClick() {
 
     console.log("Email", this.loginForm.value.email);
     console.log("Password", this.loginForm.value.password);
+
+    let response = await fetch("https://senai-gpt-api.azurewebsites.net/login", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify({
+        email: this.loginForm.value.email,
+        password: this.loginForm.value.password
+      })
+    });
+
+
+    if (this.loginForm.value.email != "" && this.loginForm.value.password != "") {
+
+      let response = await fetch("https://senai-gpt-api.azurewebsites.net/login", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json"
+        },
+        body: JSON.stringify({
+          email: this.loginForm.value.email,
+          password: this.loginForm.value.password
+        })
+      });
+
+      if (response.status >= 200 && response.status <= 299) {
+        alert("Login efetuado com sucesso! Status: " + response.status)
+
+      } else {
+        alert("Erro de login! status: " + response.status)
+      }
+    } else {
+      alert("Por favor preencha os campos de login e senha")
+    }
   }
 }
+
