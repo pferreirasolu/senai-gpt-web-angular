19d8a158d753543dcc0ed71bb669862b0a2a8572
Pferreirasolu <pedrohg.ferreira@hotmail.com>
2025-09-09 20:13:05 -0300

    AuthGuard

--- src/app/user-module/login-screen/login-screen.ts
+++ src/app/user-module/login-screen/login-screen.ts
@@ -13,8 +13,10 @@ export class LoginScreen {
 
   loginForm: FormGroup;
 
-successStatusMessage:string;
-errorStatusMessage:string;
+emailErrorMessage: string;
+passwordErrorMessage: string;
+successLogin:string;
+incorrectCredentials:string;
 
   constructor(private fb: FormBuilder, private cd: ChangeDetectorRef) {
 
@@ -23,15 +25,19 @@ errorStatusMessage:string;
       password: ["", [Validators.required]]
     })
 
-    this.successStatusMessage = "";
-    this.errorStatusMessage ="";
+    this.passwordErrorMessage ="";
+    this.emailErrorMessage="";
+    this.successLogin = "";
+    this.incorrectCredentials ="";
   }
 
 
 
   async onLoginClick() {
-    this.successStatusMessage = "";
-    this.errorStatusMessage ="";
+    this.passwordErrorMessage ="";
+    this.emailErrorMessage="";
+    this.successLogin = "";
+    this.incorrectCredentials ="";
 
     let response = await fetch("https://senai-gpt-api.azurewebsites.net/login", {
       method: "POST",
@@ -59,7 +65,7 @@ errorStatusMessage:string;
       });
 
       if (response.status >= 200 && response.status <= 299) {
-       this.successStatusMessage = "Login efetuado com sucesso!";
+       this.successLogin = "Login efetuado com sucesso!";
 
        let json = await response.json();
 
@@ -73,11 +79,12 @@ errorStatusMessage:string;
 
 
        window.location.href ="chat";
+
       } else {
-        this.errorStatusMessage = "Credenciais incorretas!";
+        this.incorrectCredentials = "Credenciais incorretas!";
       }
     } else {
-      this.errorStatusMessage = "Por favor preencha os campos de login e senha";
+      this.incorrectCredentials = "Por favor preencha os campos de login e senha";
     }
     this.cd.detectChanges(); //forcar uma atualizacao da tela
   }
