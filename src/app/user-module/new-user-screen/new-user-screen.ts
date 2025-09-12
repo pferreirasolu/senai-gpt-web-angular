6ae09f23079f0e40709089aa7d549b31b57c9036
Pferreirasolu <pedrohg.ferreira@hotmail.com>
2025-09-12 19:23:32 -0300

    Tela de novo usuario

--- src/app/user-module/new-user-screen/new-user-screen.ts
+++ src/app/user-module/new-user-screen/new-user-screen.ts
@@ -1,20 +1,20 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-user-screen',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './new-user-screen.html',
   styleUrl: './new-user-screen.css'
 })        
 export class NewUserScreen {
 
-  NewUserScreenLoginForm: FormGroup;
+  newUserScreenLoginForm: FormGroup;
 
 
   constructor(private fb: FormBuilder) {
 
-    this.NewUserScreenLoginForm = this.fb.group({
+    this.newUserScreenLoginForm = this.fb.group({
       newUser: ["", [Validators.required]],
       email: ["", [Validators.required]],
       password: ["", [Validators.required]],
@@ -25,25 +25,27 @@ export class NewUserScreen {
 
   }
 
-  async onCadastro() {
-      const token = localStorage.getItem("meuToken");
+    async onCadastro() {
+        const token = localStorage.getItem("meuToken");
 
-   let response = await fetch("https://senai-gpt-api.azurewebsites.net/users", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-        "accessToken": "token"
-      },
-      body: JSON.stringify({
-        newUser: this.NewUserScreenLoginForm.value.email,
-        email: this.NewUserScreenLoginForm.value.email,
-        password: this.NewUserScreenLoginForm.value.password,
-        newPassword: this.NewUserScreenLoginForm.value.password
-      })
-    });
+    let response = await fetch("https://senai-gpt-api.azurewebsites.net/users", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+          "authorization": `Bearer ${token}`
+        },
+        body: JSON.stringify({
+          newUser: this.newUserScreenLoginForm.value.newUser,
+          email: this.newUserScreenLoginForm.value.email,
+          password: this.newUserScreenLoginForm.value.password,
+          newPassword: this.newUserScreenLoginForm.value.newPassword
+        })
+      });
 
+
+
+    }
 
   }
-  
 
-  }
+
