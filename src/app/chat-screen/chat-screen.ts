b88b00e168ecffa0c40a67bb1fc4d3850687794c
Pferreirasolu <pedrohg.ferreira@hotmail.com>
2025-09-15 21:50:39 -0300

    listagem dos chats na tela do chat

--- src/app/chat-screen/chat-screen.ts
+++ src/app/chat-screen/chat-screen.ts
@@ -1,11 +1,47 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+
+
+interface IChat {
+  chatTitle: string;
+  id: number;
+  userId: string
+}
 
 @Component({
   selector: 'app-chat-screen',
-  imports: [],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './chat-screen.html',
   styleUrl: './chat-screen.css'
 })
 export class ChatScreen {
 
+  chats: IChat[];
+
+  constructor(private http: HttpClient) {
+    this.chats = [];
+
+  }
+
+  ngOnInit() {
+    this.getChats();
+
+  }
+
+  async getChats() {
+    let response = await this.http.get("https://senai-gpt-api.azurewebsites.net/chats", {
+      headers: {
+        "Authorization": "Bearer " + localStorage.getItem("meuToken")
+      }
+    }).toPromise();
+
+    if (response) {
+      this.chats = response as [];
+
+    } else {
+      console.log("Chat", response)
+    }
+
+  }
 }
